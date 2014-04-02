ca900e360fbb77b35285208f4e27fb8c22169a54
Coburn Berry <coburn.d.berry@gmail.com>
2014-04-01 22:59:48 -0700

    crazy bz's

--- bullet.js
+++ bullet.js
@@ -19,9 +19,10 @@
       ctx.beginPath();
 	  
 	  ctx.moveTo( this.pos[0] , this.pos[1] );
-      ctx.quadraticCurveTo(
+      ctx.bezierCurveTo(
 		  (Math.abs(this.startX-this.pos[0])*this.vel[0]*0.5),
 		  (Math.abs(this.startY-this.pos[1])*this.vel[1]*0.5),
+		  300,300,
 		  this.startX,this.startY);
 		  
 	  ctx.stroke();
